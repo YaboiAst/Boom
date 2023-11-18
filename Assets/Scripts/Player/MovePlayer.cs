0fd817c97d9ea8a5720c42df9b1f96df89b9ef8e
Ast <93723066+YaboiAst@users.noreply.github.com>
2023-11-18 16:50:23 -0300

    Dash and Jump Implemented

--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -10,13 +10,16 @@ public class MovePlayer : MonoBehaviour
     private CharacterController playerController;
 
     [Header("Dash")]
-    [SerializeField] private float dashForce = 10;
+    [SerializeField] private float dashForce = 3;
+    [SerializeField] private float dashTime = 0.25f;
+    private float dashTimeCounter = 0f;
     [SerializeField] private int maxDashCharges = 2;
     private int dashCharges = 2;
     [SerializeField] private float dashCooldown = 2.5f;
-    //private int dashesToCharge = 0;
     private float dashCooldownCounter = 0f;
-    private bool isDashing;
+
+    [Header("Jumping")]
+    [SerializeField] private float jumpForce = 20f;
 
     [Header("Gravity")]
     [SerializeField] private float g = -9.81f;
@@ -25,9 +28,6 @@ public class MovePlayer : MonoBehaviour
     private Vector3 velocity;
     private bool isGrounded;
 
-    // [Header("Jumping")]
-    // [SerializeField] private float jumpForce = 50f;
-    // private bool jump;
 
     void Start(){
         playerController = GetComponent<CharacterController>();
@@ -37,13 +37,16 @@ public class MovePlayer : MonoBehaviour
     void myInput(){
         movInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * g);
+        }
+
         if(Input.GetKeyDown(KeyCode.LeftShift) && movInput != Vector2.zero)
-            if(dashCharges > 0)
-                isDashing = true;
+            if(dashCharges > 0){
+                dashCharges -= 1;
+                dashTimeCounter = dashTime;
+            }
         
-        // if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
-        //     jump = true;
-        // }
     }
 
     void Update()
@@ -72,18 +75,13 @@ public class MovePlayer : MonoBehaviour
         Vector3 moveDirection = transform.right * movInput.x + transform.forward * movInput.y;
         playerController.Move(moveDirection * baseSpeed * Time.deltaTime);
 
-        if(isDashing){
+        if(dashTimeCounter > 0){
             playerController.Move(moveDirection * dashForce * 10f * Time.deltaTime);
-            dashCharges -= 1;
-            if(dashCooldownCounter <= 0){
-                dashCooldownCounter = dashCooldown;
-            }
-            isDashing = false;
+            dashTimeCounter -= Time.deltaTime;
         }
-
-        // if(jump && isGrounded){
-        //     playerController.Move(transform.up * jumpForce * 10f * Time.deltaTime);
-        //     jump = false;
-        // }
+        else if(dashCooldownCounter <= 0){
+            if(dashCharges == maxDashCharges)
+                dashCooldownCounter = dashCooldown;
+        }
     }
 }
