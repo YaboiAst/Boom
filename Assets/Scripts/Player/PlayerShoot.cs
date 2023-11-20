9319ee88f5865145e33dbc9977d15ddbb9e5d822
Ast <93723066+YaboiAst@users.noreply.github.com>
2023-11-20 17:45:55 -0300

    Shooting VFX

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Shapes2D;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,6 +11,10 @@ public class PlayerShoot : MonoBehaviour
     private bool isShooting = false;
     [SerializeField] private float critMultiplier = 2f;
     private float cooldownCounter = 0f;
+
+    public GameObject bullet;
+    public Transform gunEnd;
+    private Vector3 shotDirection;
 
     public Camera mainCam;
     private Vector3 aim;
@@ -46,6 +51,11 @@ public class PlayerShoot : MonoBehaviour
         float x = Random.Range(-gun.spread, gun.spread);
         float y = Random.Range(-gun.spread, gun.spread);
         Vector3 spreading = aim + new Vector3(x, y, 0f);
+
+        shotDirection = mainCam.ScreenToWorldPoint(spreading) + mainCam.transform.forward * 100f - gunEnd.position;
+        GameObject bul = Instantiate(bullet, gunEnd.position, transform.rotation);
+        Destroy(bul, 5f);
+        bul.GetComponent<Rigidbody>().AddForce(shotDirection * 1000f);
 
         ray = mainCam.ScreenPointToRay(spreading);
         RaycastHit hit;
